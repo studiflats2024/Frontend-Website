c2d1672b45dca27752c3863a52298dca2fcd8a90
EngAlaaKamal <alaa.kamal.ghonem@gmail.com>
2024-06-15 17:18:14 +0300

    add footer

---
+++ src/app/components/homeAPP/footer/footer.component.ts
@@ -0,0 +1,24 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-footer',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './footer.component.html',
+//   styleUrl: './footer.component.css'
+// })
+// export class FooterComponent {
+// }
+
+
+import { Component , EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  
+}
+
--- src/app/components/homeAPP/homeAPP.module.ts
+++ src/app/components/homeAPP/homeAPP.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { HomeAPPRoutingModule } from './homeAPP-routing.module';
@@ -8,6 +9,7 @@ import { HomeAPPRoutingModule } from './homeAPP-routing.module';
 @NgModule({
   declarations: [
     HeaderComponent,
+    FooterComponent,
     HomeComponent,
     SearchBarComponent
   ],
