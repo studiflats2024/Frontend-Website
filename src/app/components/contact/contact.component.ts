98833c044471dc2f3952eb03628ddac2a3134867
EngAlaaKamal <alaa.kamal.ghonem@gmail.com>
2024-08-27 17:07:02 +0300

    add contact api

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,17 +1,10 @@
-// import { Component } from '@angular/core';
 
-// @Component({
-//   selector: 'app-contact',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './contact.component.html',
-//   styleUrl: './contact.component.css'
-// })
-// export class ContactComponent {
-
-// }
 
 import { Component , EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../src/environments/environment';
+import {  MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-contact',
@@ -20,4 +13,36 @@ import { Component , EventEmitter, Output } from '@angular/core';
 })
 export class ContactComponent {
 
+
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient,  private messageService: MessageService) {
+    this.contactForm = this.fb.group({
+      sender_Name: ['', Validators.required],
+      sender_Phone: ['', Validators.required],
+      sender_Mail: ['', [Validators.required, Validators.email]],
+      sender_Message: ['', Validators.required]
+    });
+  }
+  // get senderName() {
+  //   return this.contactForm.get('sender_Name')?.invalid ?? false;
+  // }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.http.post<any>(`${environment.apiUrl + '/ApartmentV2/ContactUs'}` , this.contactForm.value).subscribe(
+        response => {
+          this.messageService.add({ severity: 'success', summary: 'Confirmed', detail: response.message });
+          console.log('Message sent successfully', response);
+
+        },
+        error => {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: error.error });
+          console.error('Error sending message', error);
+        }
+      );
+    } else {
+      console.error('Form is not valid');
+    }
+  }
 }
--- src/app/components/contact/contact.module.ts
+++ src/app/components/contact/contact.module.ts
@@ -3,6 +3,14 @@ import { NgModule ,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+
+
 // import { FooterComponent } from '../homeAPP/footer/footer.component';
 
 const routes: Routes = [
@@ -13,8 +21,12 @@ const routes: Routes = [
   declarations: [ContactComponent ],
   imports: [
     CommonModule,
+    ToastModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forChild(routes)
   ],
+  providers: [MessageService],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ContactModule { }
