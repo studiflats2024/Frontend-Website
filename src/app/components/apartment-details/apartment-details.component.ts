7234c5bf19f67e511271a2e914e9d30602f357ac
Alaa Kamal <alaa.kamal.ghonem@gmail.com>
2025-01-09 11:26:40 +0200

    fix some bugs

--- src/app/components/apartment-details/apartment-details.component.ts
+++ src/app/components/apartment-details/apartment-details.component.ts
@@ -375,15 +375,37 @@ activeStep2:boolean=false;
     let totalPrice = 0;
 this.roomsStep2.forEach((room: any) => {
   // totalPrice += Number(room.bed_Price) || 0;
-  totalPrice += Number(room.bed_SecuirtyDeposit) || 0;
-  totalPrice += Number(room.bed_Service_Fees) || 0;
+  // totalPrice += Number(room.bed_SecuirtyDeposit) || 0;
+  // totalPrice += Number(room.bed_Service_Fees) || 0;
   this.selectedBeds.forEach((bed: any) => {
     if (room.room_ID === bed.room_ID) {
       totalPrice += room.bed_Price || 0;
+      totalPrice +=room.bed_SecuirtyDeposit||0
+      totalPrice +=room.bed_Service_Fees||0
     }
   });
 });
 this.totalPriceBooking = totalPrice;
+console.log(this.totalPriceBooking)
+////////////////////////////////////////////////////full apart
+if(this.selectfullaprt){
+  this.selectedRooms.forEach((room: any) => {
+    // totalPrice += Number(room.bed_Price) || 0;
+    // totalPrice += Number(room.bed_SecuirtyDeposit) || 0;
+    // totalPrice += Number(room.bed_Service_Fees) || 0;
+    this.selectedBeds.forEach((bed: any) => {
+      if (room.room_ID === bed.room_ID) {
+        totalPrice += room.bed_Price || 0;
+        totalPrice +=room.bed_SecuirtyDeposit||0
+        totalPrice +=room.bed_Service_Fees||0
+      }
+    });
+  });
+  this.totalPriceBooking = totalPrice;
+  console.log(this.totalPriceBooking)
+}
+////////////////////////////////////////////////////full apart
+
 
 if(this.selectfullaprt && this.selectedItem>1 && this.selectedBeds.length>this.selectedItem ){
 
